142ed649ad5bfb27754e5fd0cf12e193b2cb5d18
Dinesh-kumar-M-2002 <deenukuber333@gmail.com>
2024-04-06 09:43:17 +0530

    add dark mode

---
+++ app/Providers.jsx
@@ -0,0 +1,11 @@
+'use client'
+import {ThemeProvider} from 'next-themes';
+export default function Providers({children}) {
+  return (
+    <ThemeProvider defaultTheme='system' attribute='class'>
+    <div className='min-h-screen select-none transition-colors duration-300'>
+       {children} 
+    </div>
+    </ThemeProvider>
+  )
+}
